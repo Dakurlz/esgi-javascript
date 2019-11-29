37b4af653c9abaa49d0b3247043724e00dd5e0d9
Gatay Bryan <gatay.bryan@gmail.com>
2019-11-29 14:45:42 +0100

    done 2-2

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -5,3 +5,29 @@ function type_check_v1(value,type){
     else return false
 
 }
+
+function type_check_v2(value,type) {
+    if (typeof value == type.type) {
+        if (type.value) {
+            if (type.value === value) {
+                return true
+            } else {
+                return false
+            }
+        }else{
+            return true
+        }
+        } else if (type.enum) {
+            if (type.enum.includes(value)) {
+                return true
+            }
+        } else {
+            return false
+        }
+
+}
+
+console.log(type_check_v2({prop1:1},{type:"object"}))
+console.log(type_check_v2("foo",{type:"string",value:"foo"}))
+console.log(type_check_v2("bar",{type:"string",value:"foo"}))
+console.log(type_check_v2(3,{enum:["foo","bar",3]}))
